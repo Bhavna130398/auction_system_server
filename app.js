5223c1cecf5bb9a5abadfc9a408bdb403897c0fe
mayurugale <mayurugale517@gmail.com>
2021-04-07 21:36:46 +0530

    bidder/seller code added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var expressMongoDb = require('express-mongo-db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(expressMongoDb('mongodb://localhost/auctionDB'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/admin.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+
+router.post('/bidderList', function (req, res, next) {
+    req.db.collection("bidder").findOne({}, function (err, result) {
+        if (err)
+            res.json({});
+        else {
+            res.json(result);
+        }
+    });
+});
+
+router.post('/sellerList', function (req, res, next) {
+    req.db.collection("seller").findOne({}, function (err, result) {
+        if (err)
+            res.json({});
+        else {
+            res.json(result);
+        }
+    });
+});
+
+
+module.exports = router;
